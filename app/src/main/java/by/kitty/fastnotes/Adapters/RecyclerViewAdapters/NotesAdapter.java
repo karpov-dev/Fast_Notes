98141ca6301d8df0884cd2e229bd367f70a7fcda
karpov-dev <passangerfeat@gmail.com>
2020-12-11 03:28:23 +0300

    0.0.5 - Added Adapter for Notes

---
+++ app/src/main/java/by/kitty/fastnotes/Adapters/RecyclerViewAdapters/NotesAdapter.java
@@ -0,0 +1,52 @@
+package by.kitty.fastnotes.Adapters.RecyclerViewAdapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import by.kitty.fastnotes.Model.Note;
+import by.kitty.fastnotes.R;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> {
+    private List<Note> notes;
+    private LayoutInflater inflater;
+
+    public NotesAdapter(Context context, List<Note> notes) {
+        this.notes = notes;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public NotesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.note_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesAdapter.ViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.nameView.setText(note.getId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView nameView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.nameView = (TextView) itemView.findViewById(R.id.name);
+        }
+    }
+}
